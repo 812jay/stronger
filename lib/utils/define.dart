471296874532a6da48c0012284e82edcb95115ae
jay <yongje812@gmail.com>
2021-12-14 03:51:26 +0900

    define 색상 추가

--- lib/utils/define.dart
+++ lib/utils/define.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+
 class ColorsStronger {
   ColorsStronger._();
+
+  static const primaryBG = Color(0xFF575fcf);
+  static const lightGreen = Color(0xFF05c46b);
+  static const lightGrey = Color(0xFF9e9e9e);
 }
